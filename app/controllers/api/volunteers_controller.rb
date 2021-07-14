922f79ed3729f137ebe28f3c5c31d28e6d290da5
kylefarmer85 <kylefarmer85@gmail.com>
2021-07-13 22:11:03 -0500

    create VolunteersController show action, create volunteers show route

--- app/controllers/api/volunteers_controller.rb
+++ app/controllers/api/volunteers_controller.rb
@@ -1,2 +1,12 @@
 class Api::VolunteersController < ApplicationController
+
+  def show
+    volunteer = Volunteer.find(params[:id])
+    if volunteer
+      render json: volunteer
+    else
+      render json: {error: "Volunteer not found"}
+    end
+  end  
+  
 end
