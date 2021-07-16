dd437dc2c8592d7d91d7b137612313569345bc19
kylefarmer85 <kylefarmer85@gmail.com>
2021-07-15 20:14:43 -0500

    fix error handling for when volunteer id record not found

--- app/controllers/api/v1/volunteers_controller.rb
+++ app/controllers/api/v1/volunteers_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::VolunteersController < ApplicationController
   # GET /api/v1/volunteers/:id
   def show
     volunteer = Volunteer.find(params[:id])
+    rescue ActiveRecord::RecordNotFound 
     if volunteer
       render json: volunteer
     else
