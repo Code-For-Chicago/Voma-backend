b572f3dedba82006b2b3a51feb4bf2da57269c9e
Ryan Koch <4325613+Ryan-Koch@users.noreply.github.com>
2021-07-19 17:58:51 -0400

    Create create volunteers endpoint. Define spec for endpoint. Add model validation.

---
+++ spec/controllers/volunteers_controller_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe "API V1 Volunteers", type: :request do
+    describe "POST /api/v1/volunteers" do
+        context "with valid parameters" do 
+            let(:valid_params) do
+                {
+                    name: 'Test name',
+                    email: 'test@example.com',
+                    slackUserId: 'slkdfjas39230523',
+                    pronouns: "they/them",
+                    primarySkills: "development",
+                    secondarySkills: "facilitation",
+                    employer: "test employer",
+                    jobTitle: "developer"
+                }
+            end
+
+            it "creates a new volunteer" do
+                expect { post "/api/v1/volunteers", params: valid_params}.to change(Volunteer, :count).by(+1)
+                expect(response).to have_http_status :created
+                expect(Volunteer.last).to have_attributes valid_params
+            end
+        end
+        context "missing required model parameter" do 
+            # we're missing the name field here
+            let(:invalid_params) do {
+                email: 'test@example.com',
+                slackUserId: 'slkdfjas39230523',
+                pronouns: "they/them",
+                primarySkills: "development",
+                secondarySkills: "facilitation",
+                employer: "test employer",
+                jobTitle: "developer"
+            }
+        end
+            it "fails to createa a new volunteer" do
+                expect { post "/api/v1/volunteers", params: invalid_params}.to change(Volunteer, :count).by(0)
+                expect(response).to have_http_status :bad_request 
+            end 
+        end
+    end
+end
